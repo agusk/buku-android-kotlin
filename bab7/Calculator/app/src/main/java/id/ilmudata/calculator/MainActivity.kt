867f2f6cdee5b1ff6baa26a1f33041c3db912cfc
Agus Kurniawan <agusk@users.noreply.github.com>
2021-11-08 22:16:46 +0700

    updated

--- bab7/Calculator/app/src/main/java/id/ilmudata/calculator/MainActivity.kt
+++ bab7/Calculator/app/src/main/java/id/ilmudata/calculator/MainActivity.kt
@@ -3,81 +3,89 @@ package id.ilmudata.calculator
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import id.ilmudata.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    var actPlus: Boolean = false;
-    var actMultiply: Boolean = false;
-    var actSubract: Boolean = false;
-    var actDivide: Boolean = false;
-    var valueOne: Float = 0f;
-    var valueTwo: Float = 0f
+    private lateinit var binding: ActivityMainBinding
+
+    private var actPlus: Boolean = false
+    private var actMultiply: Boolean = false
+    private var actSubract: Boolean = false
+    private var actDivide: Boolean = false
+    private var valueOne: Float = 0f
+    private var valueTwo: Float = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     fun calculate(v: View){
+        var text = binding.txtResult.text.toString()
+        if(text=="0"){
+            text = ""
+        }
         when(v.id) {
-            R.id.btn0 -> txtResult.setText(txtResult.text.toString() + "0")
-            R.id.btn1 -> txtResult.setText(txtResult.text.toString() + "1")
-            R.id.btn2 -> txtResult.setText(txtResult.text.toString() + "2")
-            R.id.btn3 -> txtResult.setText(txtResult.text.toString() + "3")
-            R.id.btn4 -> txtResult.setText(txtResult.text.toString() + "4")
-            R.id.btn5 -> txtResult.setText(txtResult.text.toString() + "5")
-            R.id.btn6 -> txtResult.setText(txtResult.text.toString() + "6")
-            R.id.btn7 -> txtResult.setText(txtResult.text.toString() + "7")
-            R.id.btn8 -> txtResult.setText(txtResult.text.toString() + "8")
-            R.id.btn9 -> txtResult.setText(txtResult.text.toString() + "9")
-            R.id.btnC -> txtResult.setText("")
-            R.id.btnD -> txtResult.setText(txtResult.text.toString() + ".")
+            R.id.btn0 -> binding.txtResult.text = text.plus("0")
+            R.id.btn1 -> binding.txtResult.text = text.plus("1")
+            R.id.btn2 -> binding.txtResult.text = text.plus("2")
+            R.id.btn3 -> binding.txtResult.text = text.plus( "3")
+            R.id.btn4 -> binding.txtResult.text = text.plus("4")
+            R.id.btn5 -> binding.txtResult.text = text.plus("5")
+            R.id.btn6 -> binding.txtResult.text = text.plus("6")
+            R.id.btn7 -> binding.txtResult.text = text.plus("7")
+            R.id.btn8 -> binding.txtResult.text = text.plus("8")
+            R.id.btn9 -> binding.txtResult.text = text.plus("9")
+            R.id.btnC -> binding.txtResult.text = ""
+            R.id.btnD -> binding.txtResult.text = text.plus(".")
             R.id.btnPlus -> {
-                valueOne = txtResult.text.toString().toFloat();
-                actPlus = true;
-                txtCache.setText(valueOne.toString());
-                txtResult.setText(null);
+                valueOne = binding.txtResult.text.toString().toFloat()
+                actPlus = true
+                binding.txtCache.text = valueOne.toString()
+                binding.txtResult.text = "0"
             }
             R.id.btnDivide -> {
-                valueOne = txtResult.text.toString().toFloat();
-                actDivide = true;
-                txtCache.setText(valueOne.toString());
-                txtResult.setText(null);
+                valueOne = binding.txtResult.text.toString().toFloat()
+                actDivide = true
+                binding.txtCache.text = valueOne.toString()
+                binding.txtResult.text = "0"
             }
             R.id.btnMultiply -> {
-                valueOne = txtResult.text.toString().toFloat();
-                actMultiply = true;
-                txtCache.setText(valueOne.toString());
-                txtResult.setText(null);
+                valueOne = binding.txtResult.text.toString().toFloat()
+                actMultiply = true
+                binding.txtCache.text = valueOne.toString()
+                binding.txtResult.text = "0"
             }
             R.id.btnSubtract -> {
-                valueOne = txtResult.text.toString().toFloat();
-                actSubract = true;
-                txtCache.setText(valueOne.toString());
-                txtResult.setText(null);
+                valueOne = binding.txtResult.text.toString().toFloat()
+                actSubract = true
+                binding.txtCache.text = valueOne.toString()
+                binding.txtResult.text = "0"
             }
             R.id.btnEqual -> {
-                valueTwo = txtResult.text.toString().toFloat();
+                valueTwo = binding.txtResult.text.toString().toFloat()
                 if(actPlus){
-                    txtResult.setText((valueOne + valueTwo).toString() + "");
-                    txtCache.setText(null);
-                    actPlus = false;
+                    binding.txtResult.text = ((valueOne + valueTwo).toString() + "")
+                    binding.txtCache.text = ""
+                    actPlus = false
                 }
                 if(actDivide){
-                    txtResult.setText((valueOne / valueTwo).toString() + "");
-                    txtCache.setText(null);
-                    actDivide = false;
+                    binding.txtResult.text = ((valueOne / valueTwo).toString() + "")
+                    binding.txtCache.text = ""
+                    actDivide = false
                 }
                 if(actMultiply){
-                    txtResult.setText((valueOne * valueTwo).toString() + "");
-                    txtCache.setText(null);
-                    actMultiply = false;
+                    binding.txtResult.text = ((valueOne * valueTwo).toString() + "")
+                    binding.txtCache.text = ""
+                    actMultiply = false
                 }
                 if(actSubract){
-                    txtResult.setText((valueOne - valueTwo).toString() + "");
-                    txtCache.setText(null);
-                    actSubract = false;
+                    binding.txtResult.text = ((valueOne - valueTwo).toString() + "")
+                    binding.txtCache.text = ""
+                    actSubract = false
                 }
             }
         }
